e45eba53573bd2422064dfd42509f6ee64379c56
Jesse Nelson <jnels124@msudenver.edu>
2013-07-19 02:59:32 -0600

    Set up repo for awesome

---
+++ obvious1/init.lua
@@ -0,0 +1,14 @@
+-------------------------------------------
+-- Author: Gregor "farhaven" Best"       --
+-- Copyright 2009 Gregor Best            --
+-------------------------------------------
+
+require("obvious.lib")
+require("obvious.clock")
+require("obvious.battery")
+require("obvious.volume_alsa")
+require("obvious.wlan")
+require("obvious.popup_run_prompt")
+require("obvious.basic_mpd")
+
+module("obvious")
---
+++ obvious1/wlan/init.lua
@@ -0,0 +1,69 @@
+--------------------------------
+-- Author: Gregor Best        --
+-- Copyright 2009 Gregor Best --
+--------------------------------
+
+local setmetatable = setmetatable
+local tonumber = tonumber
+
+local io = {
+    open = io.open
+}
+local string = {
+    format = string.format
+}
+local capi = {
+    widget = widget
+}
+
+local awful = require("awful")
+local lib = require("obvious.lib")
+
+module("obvious.wlan")
+
+widget = capi.widget({
+    type = "textbox",
+    name = "tb_wlan",
+    align = "right"
+})
+device = "wlp2s0"
+
+function get_data()
+    local rv = { }
+
+    local fd = io.open("/proc/net/wireless")
+    if not fd then return end
+
+    for line in fd:lines() do
+        if line:match("^ "..device) then
+            rv.link = tonumber(line:match("   (%d?%d?%d)"))
+            break
+        end
+    end
+    fd:close()
+    if not rv.link then return end
+
+    return rv
+end
+
+local function update()
+    local status = get_data()
+
+    local color = "#009000"
+    if status.link < 50 and status.link > 10 then
+        color = "#909000"
+    elseif status.link <= 10 then
+        color = "#900000"
+    end
+    widget.text = lib.util.colour(color,"☢") .. string.format(" %03d%%", status.link)
+end
+update()
+lib.hooks.timer.register(10, 60, update)
+lib.hooks.timer.start(update)
+
+function set_device(dev)
+    device = dev
+    update()
+end
+
+setmetatable(_M, { __call = function () return widget end })
